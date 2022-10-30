5f872421a8290d5197caa691a45e208b3403b5f2
Petyo Ivanov <underlog@gmail.com>
2022-10-30 11:07:09 +0200

    inverse iframe and custom component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,21 @@
-import { DOMAttributes, useState } from "react";
-
-if (typeof HTMLElement !== "undefined") {
-  class CustomComponent extends HTMLElement {
-    connectedCallback() {
-      console.log("foo");
-
-      // create a shadow dom
-      const shadow = this.attachShadow({ mode: "open" });
-      // create a span element
-      const text = document.createElement("span");
-      // set the content to 'Hello World'
-
-      const iframe = document.createElement("iframe");
-      iframe.src = "sidebar";
-      //@ts-expect-error
-      iframe.style = "height: 90vh; position: absolute; right: 20px; border: 0";
-      // insert our created element into our shadow DOM, causing it to appear
-      shadow.appendChild(iframe);
-    }
-  }
-
-  customElements.define("custom-component", CustomComponent);
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      ["custom-component"]: any;
-    }
-  }
-}
+import { useState } from "react";
 
 export default function Home() {
   const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div>
       <button onClick={() => setShowSidebar(true)}>Open sidebar</button>
-      {showSidebar && <custom-component></custom-component>}
+      {showSidebar && (
+        <iframe
+          src="sidebar"
+          style={{
+            height: "90vh",
+            position: "absolute",
+            right: "20px",
+            border: 0,
+          }}
+        />
+      )}
     </div>
   );
 }
--- pages/sidebar.tsx
+++ pages/sidebar.tsx
@@ -1,13 +1,34 @@
 import { Virtuoso } from "react-virtuoso";
+import ReactDOM from "react-dom/client";
+
+if (typeof HTMLElement !== "undefined") {
+  class CustomComponent extends HTMLElement {
+    connectedCallback() {
+      const mountPoint = document.createElement("div");
+      this.attachShadow({ mode: "open" }).appendChild(mountPoint);
+
+      const root = ReactDOM.createRoot(mountPoint);
+      root.render(
+        <Virtuoso
+          style={{ height: "100vh" }}
+          totalCount={200}
+          itemContent={(index) => <div>Item {index}</div>}
+        />
+      );
+    }
+  }
+
+  customElements.define("custom-component", CustomComponent);
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      ["custom-component"]: any;
+    }
+  }
+}
 
 export default function Sidebar() {
-  return (
-    <div>
-      <Virtuoso
-        style={{ height: "100vh" }}
-        totalCount={200}
-        itemContent={(index) => <div>Item {index}</div>}
-      />
-    </div>
-  );
+  return <custom-component></custom-component>;
 }
